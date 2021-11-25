9cf3af0b97828c54af96a88bd6ab4a0e6b71d56d
Sk031397 <sk031397.scholar@gmail.com>
2021-11-25 13:53:07 +0800

    me

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,11 @@ import facedetection from './img/face_detection.jpg'
 import eyedetection from './img/eye_detection.jpg';
 function App() {
   const videoRef = useRef(null);
+  const [isFaceDetected,setIsFaceDetected] = useState(true);
+  const [isFaceMeshed,setIsFaceMeshed] = useState(true);
+  const [isPosed,setIsPosed] = useState(true);
+  const [isObjectDetected,setIsObjectDetected] = useState(true);
+  const [isEyeDetected,setIsEyeDetected] = useState(true);
   const getVideo = () => {
     navigator.mediaDevices.getUserMedia({
       video:{
@@ -23,6 +28,26 @@ function App() {
       console.error(err);
     })
   }
+  const OnFaceDetected = () => {
+    isFaceDetected ? setIsFaceDetected(true) : setIsFaceDetected(false);
+    console.log("isFaceDetected: ", isFaceDetected);
+  }
+  const OnFaceMeshed = () => {
+    isFaceMeshed ? setIsFaceMeshed(true) : setIsFaceMeshed(false);
+    console.log("isFaceMeshed: ", isFaceMeshed);
+  }
+  const OnEyeDetected = () => {
+    isEyeDetected ? setIsEyeDetected(true) : setIsEyeDetected(false);
+    console.log("isEyeDetected: ", isEyeDetected);
+  }
+  const OnObjectDetected = () => {
+    isObjectDetected ? setIsObjectDetected(true) : setIsObjectDetected(false);
+    console.log("isObjectDetected",isObjectDetected);
+  }
+  const OnPosed = () => {
+    isPosed ? setIsPosed(true): setIsPosed(false);
+    console.log("isPosed",isPosed);
+  }
   useEffect(()=> {
     getVideo();
   },[videoRef])
@@ -33,24 +58,36 @@ function App() {
           <div className="video_container">
             <video ref={videoRef}></video>
               <div className="main_buttons">
-            <button>
-              <img src={objectdetection}/>
-            </button>
-
-            <button>
-              <img src={facemesh}/>
-            </button>
-            <button>
-              <img src={facedetection}/>
-            </button>
-
-            <button>
-              <img src={pose_estimation}/>
-            </button>
-
-            <button>
-              <img src={eyedetection} />
-            </button>
+              {
+                isObjectDetected &&
+                <button onClick={() => OnObjectDetected()}>
+                  <img src={objectdetection}/>
+                </button> 
+              }
+              {
+                isFaceMeshed &&
+                <button onClick={() => OnFaceDetected()}>
+                  <img src={facedetection}/>
+                </button>
+              }
+              {
+                isFaceDetected &&
+                <button onClick={() => OnFaceDetected()}>
+                  <img src={facemesh}/>
+                </button>
+              }
+              {
+                isPosed && 
+                <button onClick={() => OnPosed()}>
+                  <img src={pose_estimation}/>
+                </button>
+              }
+              {
+                isEyeDetected &&
+                <button onClick={()=> OnEyeDetected()}>
+                  <img src={eyedetection} />
+               </button>
+              }
         </div>
       </div>
     </div>
